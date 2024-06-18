c22ad3f071234691d1c0facdd5ab1f25c60cfeb9
Chin-Wei Huang <cw.huang427@gmail.com>
2024-06-18 19:23:54 +0200

    Update test_q.py to catch shape errors

--- tests/test_q.py
+++ tests/test_q.py
@@ -19,7 +19,12 @@ def test_mean(ddpm: DDPM, ddpm_gt: GT):
     
     z = torch.ones(2, 1)
     t = torch.ones(2, dtype=torch.long)
-    assert torch.allclose(ddpm._q_mean(z, t), ddpm_gt._q_mean(z, t)), (
+    q_mean = ddpm._q_mean(z, t)
+    q_mean_gt = ddpm_gt._q_mean(z, t)
+    assert q_mean.shape == q_mean_gt.shape, (
+        f"Incorrect _q_mean shape. Expecting {q_mean_gt.shape}, got {q_mean.shape}"
+    )
+    assert torch.allclose(q_mean, q_mean_gt), (
         "_q_mean is incorrectly implemented"
     )
     
@@ -28,6 +33,11 @@ def test_std(ddpm: DDPM, ddpm_gt: GT):
     
     z = torch.ones(2, 1)
     t = torch.ones(2, dtype=torch.long)
-    assert torch.allclose(ddpm._q_std(z, t), ddpm_gt._q_std(z, t)), (
+    q_std = ddpm._q_std(z, t)
+    q_std_gt = ddpm_gt._q_std(z, t)
+    assert q_std.shape == q_std_gt.shape, (
+        f"Incorrect _q_std shape. Expecting {q_std_gt.shape}, got {q_std.shape}"
+    )
+    assert torch.allclose(q_std, q_std_gt), (
         "_q_std is incorrectly implemented"
     ) 
